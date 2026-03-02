e22d6b6c25dabf4b2506e77b950626095302d017
wangli <1925254338@qq.com>
2026-03-02 14:12:52 +0800

    update CMakeLists and sh

--- download_models.sh
+++ download_models.sh
@@ -15,22 +15,24 @@ mkdir -p $AX630C_PATH/$KOKORO_PATH
 export HF_ENDPOINT=https://hf-mirror.com
 
 # kokoro
-hf download AXERA-TECH/kokoro.axera models/kokoro_part1_96.axmodel --local-dir $AX650_PATH/$KOKORO_PATH
-hf download AXERA-TECH/kokoro.axera models/kokoro_part2_96.axmodel --local-dir $AX650_PATH/$KOKORO_PATH
-hf download AXERA-TECH/kokoro.axera models/kokoro_part3_96.axmodel --local-dir $AX650_PATH/$KOKORO_PATH
-hf download AXERA-TECH/kokoro.axera models/model4_har_sim.onnx --local-dir $AX650_PATH/$KOKORO_PATH
-hf download AXERA-TECH/kokoro.axera models_620E/kokoro_part1_96.axmodel --local-dir $AX630C_PATH/$KOKORO_PATH
-hf download AXERA-TECH/kokoro.axera models_620E/kokoro_part2_96.axmodel --local-dir $AX630C_PATH/$KOKORO_PATH
-hf download AXERA-TECH/kokoro.axera models_620E/kokoro_part3_96.axmodel --local-dir $AX630C_PATH/$KOKORO_PATH
-hf download AXERA-TECH/kokoro.axera models_620E/model4_har_sim.onnx --local-dir $AX630C_PATH/$KOKORO_PATH
+hf download AXERA-TECH/kokoro.axera --include models/kokoro_part1_96.axmodel --local-dir $AX650_PATH/$KOKORO_PATH
+hf download AXERA-TECH/kokoro.axera --include models/kokoro_part2_96.axmodel --local-dir $AX650_PATH/$KOKORO_PATH
+hf download AXERA-TECH/kokoro.axera --include models/kokoro_part3_96.axmodel --local-dir $AX650_PATH/$KOKORO_PATH
+hf download AXERA-TECH/kokoro.axera --include models/model4_har_sim.onnx --local-dir $AX650_PATH/$KOKORO_PATH
+hf download AXERA-TECH/kokoro.axera --include models_620E/kokoro_part1_96.axmodel --local-dir $AX630C_PATH/$KOKORO_PATH
+hf download AXERA-TECH/kokoro.axera --include models_620E/kokoro_part2_96.axmodel --local-dir $AX630C_PATH/$KOKORO_PATH
+hf download AXERA-TECH/kokoro.axera --include models_620E/kokoro_part3_96.axmodel --local-dir $AX630C_PATH/$KOKORO_PATH
+hf download AXERA-TECH/kokoro.axera --include models_620E/model4_har_sim.onnx --local-dir $AX630C_PATH/$KOKORO_PATH
 mv $AX650_PATH/$KOKORO_PATH/models/* $AX650_PATH/$KOKORO_PATH
 mv $AX630C_PATH/$KOKORO_PATH/models_620E/* $AX630C_PATH/$KOKORO_PATH
 
+hf download AXERA-TECH/kokoro.axera --include cpp/voices/* --local-dir $AX650_PATH/$KOKORO_PATH
+hf download AXERA-TECH/kokoro.axera --include cpp/voices/* --local-dir $AX630C_PATH/$KOKORO_PATH
+mv $AX650_PATH/$KOKORO_PATH/cpp/voices $AX650_PATH/$KOKORO_PATH/voices
+mv $AX630C_PATH/$KOKORO_PATH/cpp/voices $AX630C_PATH/$KOKORO_PATH/voices
+
+
 cp dict/vocab.txt $AX650_PATH/$KOKORO_PATH
 cp dict/vocab.txt $AX630C_PATH/$KOKORO_PATH
-git clone https://github.com/AXERA-TECH/kokoro.axera.git --depth=1
-cp -r kokoro.axera/cpp/voices $AX650_PATH/$KOKORO_PATH
-cp -r kokoro.axera/cpp/voices $AX630C_PATH/$KOKORO_PATH
-rm -rf kokoro.axera
 
 echo "ALL DONE"
